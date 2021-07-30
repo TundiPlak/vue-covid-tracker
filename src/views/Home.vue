6e46053974c2cee3e6c607e8a95e1b19e218f223
Tundi Torok <tunde.torok.pr@gmail.com>
2021-07-30 14:13:55 +0300

    setting up CountrySelect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,21 @@
   <main v-if="!loading">
     <!--Show data-->
     <DataTitle :text="title" :dataDate="dataDate" />
+
     <DataBoxes :stats="stats" />
+
+    <CountrySelect :countries="countries" @get-country="getCountryData" />
+    <!--emit brought by @get-country, and set it to a function as of getCountryData, which will run when there is an emit -->
+
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+    >
+      Clear Country
+    </button>
   </main>
+
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
       Fetching Data
@@ -16,12 +29,14 @@
 // @ is an alias to /src
 import DataTitle from "@/components/DataTitle";
 import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
   name: "Home",
   components: {
     DataTitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -39,12 +54,24 @@ export default {
       const data = await res.json();
       return data;
     },
+    getCountryData(country) {
+      // country is coming from the emit() in CountrySelect
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
+    },
   },
   async created() {
     const data = await this.fetchCovidData();
     this.dataDate = data.Date; // Date is gathered from tha package
     this.stats = data.Global;
-    this.countries = data.countries;
+    this.countries = data.Countries;
     this.loading = false; //as true by deafult
   },
 };
